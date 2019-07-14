6dab20e33cb6f64294a466148f9e37caf1ce9c4a
Chathura Gunasekara <cjgunase@mtu.edu>
2019-07-13 23:12:47 -0500

    section4 code

---
+++ section4_code_reuse/mymodules/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 13 22:36:37 2019
+
+@author: Chathura Gunasekara
+"""
+
+from setuptools import setup
+
+setup(
+      name = 'vsearch',
+      version='1.0',
+      description = 'The HF python search tool',
+      author='HF- python 2e',
+      author_email='gunaseka@bcm.edu',
+      url = 'headfirstlabs.com',
+      py_module=['vsearch']
+      
+      
+      )
+
